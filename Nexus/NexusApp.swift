3006a57f3c78a47363e8ddd48e3dc0ac52ae5213
Murat Kolar <muratkolar@Icloud.com>
2026-02-26 22:00:47 +0100

    Initial Commit

---
+++ Nexus/NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusApp.swift
+//  Nexus
+//
+//  Created by Murat Kolar on 26.02.2026..
+//
+
+import SwiftUI
+
+@main
+struct NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
